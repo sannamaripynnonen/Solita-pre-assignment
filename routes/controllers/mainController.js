787029074464ac4c38d8d521e76db3f101daae5e
Sannamari <sannamari.pynnonen@tuni.fi>
2023-05-01 17:57:00 +0300

    Modify layout

--- routes/controllers/mainController.js
+++ routes/controllers/mainController.js
@@ -1,5 +1,13 @@
-const showMain = ({ render }) => {
-    render('index.eta')
+import * as journeyService from '../../services/journeyServices.js';
+import * as stationService from '../../services/stationServices.js';
+
+const showMain = async ({ render }) => {
+    const data = {
+        stationsEspoo: await stationService.getAllEspooStations(),
+        stationsHelsinki: await stationService.getAllHelsinkiStations(),
+        trips: await journeyService.getJourneyCount(),
+    }
+    render('index.eta', data);
 };
 
 export { showMain };
--- services/journeyServices.js
+++ services/journeyServices.js
@@ -4,8 +4,13 @@ const getJourneys = async () => {
     return await sql`SELECT dept_station, return_station, (distance / 1000) AS distance, FLOOR(duration / 60) AS duration FROM citybiketrips LIMIT 100`;
 }
 
+const getJourneyCount = async () => {
+    const trips = await sql`SELECT COUNT(*) FROM citybiketrips`
+    return trips[0];
+}
+
 const getJourneysByStation = async (station) => {
     return await sql`SELECT dept_station, return_station, (distance / 1000) AS distance, FLOOR(duration / 60) AS duration FROM citybiketrips WHERE dept_station iLIKE '%' || ${station} || '%' OR return_station iLIKE '%' || ${station} || '%' LIMIT 100`;
 }
 
-export { getJourneys, getJourneysByStation }
+export { getJourneys, getJourneyCount, getJourneysByStation }
